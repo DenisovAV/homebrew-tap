af5d16c444f668d6a3c827cbcc3b33c337773f9c
Sasha Denisov <denisov.shureg@gmail.com>
2026-09-13 16:42:58 +0200

    litetune 0.1.6

--- Formula/litetune.rb
+++ Formula/litetune.rb
@@ -3,8 +3,8 @@ class Litetune < Formula
 
   desc "Fine-tune, convert and verify small models for on-device"
   homepage "https://github.com/DenisovAV/litetune"
-  url "https://files.pythonhosted.org/packages/b0/78/3a05dd453c1dcc0ea53eb8ca495878099233f7ce2a5d00c6c43d4b7f896f/litetune-0.1.5.tar.gz"
-  sha256 "fbdbb39db6c25c91509939dcd1dc8ed645212d8e82df0e8a53e29316ec1cc340"
+  url "https://files.pythonhosted.org/packages/4a/1e/4cfe18bd49c90fecabbf45dd37c4f5428453eb8b2cf1239dd667b33dcb56/litetune-0.1.6.tar.gz"
+  sha256 "0ccb3a13b284575f618dda0d8bcb2dbc1b028b54f5eeb6207b8b8412c8582584"
   license "Apache-2.0"
 
   # `numpy==2.0.2`, pinned by the stage environments litetune provisions,
